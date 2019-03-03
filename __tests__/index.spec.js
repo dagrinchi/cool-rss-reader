bed05ac3e685d4348d38e489a4bf9c0dedfecc6d
David Alméciga <wdavid@dagrinchi.com>
2019-03-03 14:53:17 -0500

    tests(channel): Add channel tests by snapshot

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 
+const DEFAULT_CHANNELS = [
+  {title:"ESPN - NBA", url:"http://www.espn.com/espn/rss/nba/news"},
+  {title:"Washington Post - Soccer", url:"http://feeds.washingtonpost.com/rss/rss_soccer-insider"},
+]
+
 import IndexPage from '../pages/index.js'
 
 describe('Pages', () => {
   describe('Index', () => {
     it('Should match with snapshot', () => {
-      const component = renderer.create(<IndexPage />)
+      const component = renderer.create(<IndexPage channels={DEFAULT_CHANNELS} />)
       const tree = component.toJSON()
       expect(tree).toMatchSnapshot()
     })
--- components/OrChannelsGrid/OrChannelsGrid.js
+++ components/OrChannelsGrid/OrChannelsGrid.js
@@ -23,7 +23,7 @@ const OrChannelsGrid = (props) => (
       props.channels.map((c, index) => (
         <MolChannelCard
           className="channel-card"
-          key={c.slug}
+          key={index}
           title={c.title || "Lorem ipsum"}
           description={c.description || "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."}
           index={index}
